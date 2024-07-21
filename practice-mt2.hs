05d7625448debaf71b4835c7f3a97bef0baceecc
Andrew Jowe <andrewjowe@Andrews-MBP.attlocal.net>
2024-07-21 14:17:00 -0700

    short answer

--- practice-mt2.hs
+++ practice-mt2.hs
@@ -32,3 +32,21 @@ mymaximum [x] = x
 mymaximum (x1:x2:xs)
   | x1 > x2 = mymaximum (x1:xs)
   | otherwise = mymaximum (x2:xs)
+
+-- Referential transparency means that function calls are like mathematical functions and are only affected by the parameters passed in, which means that for a given set of parameters, you are always guaranteed the same output from the same function (no side effects).
+-- Functional programming considers referential transparency important because it helps boost programmer productivity and it's also what makes functional programming special over traditional languages. You give up side effects (state) for referential transparency. 
+-- Off topic: Functional programming languages also made a huge contribution to traditional languages, called lambda functions.
+
+{-
+ChatGPT answer
+In the context of functional programming, referential transparency refers to an expression [or function] that can be replaced with its corresponding value without changing the program's behavior, i.e. it always produces the same result given the same input and has no side effects
+
+Benefits of functional programming (from notes)
+1. Easier to prove correctness
+2. Testing and debugging is easier
+3. Elimination of side effects
+4. Programs abstracted away
+5. Concurrent programming easier
+6. Functions as first class objects more concise
+7. Less source code
+-}
