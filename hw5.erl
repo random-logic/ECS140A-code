ca6b47f69b5e1299b3deb3d22bc555e18c2f8478
Andrew Jowe <andrewjowe@Andrews-MBP.attlocal.net>
2024-08-02 16:41:58 -0700

    final push

--- hw5.erl
+++ hw5.erl
@@ -2,6 +2,10 @@
 -export([myremoveduplicates/1, myintersection/2, mylast/1, myreverse/1, myreplaceall/3]).
 
 % 1) myremoveduplicates
+% What source did you use? CHATGPT
+% Why did you use that source? I didn't know the syntax
+% Did using that source benefit you? Yes, I learned the syntax of pattern matching and how to use guards
+% Did using that source interfere with your understanding in any way? No
 myremoveduplicates(Str) when is_list(Str) -> myremoveduplicates(Str, []).
 
 myremoveduplicates([], Acc) -> lists:reverse(Acc);
@@ -12,6 +16,10 @@ myremoveduplicates([H|T], Acc) ->
     end.
 
 % 2) myintersection
+% What source did you use? CHATGPT
+% Why did you use that source? I was still confused on the semicolon
+% Did using that source benefit you? Yes, I learned that the semicolon separates pattern matching statements for the same function
+% Did using that source interfere with your understanding in any way? No
 myintersection(List1, List2) -> myintersection(List1, List2, []).
 
 myintersection([], _, Acc) -> lists:reverse(Acc);
@@ -27,12 +35,19 @@ mylast([H]) -> [H];
 mylast([_|T]) -> mylast(T).
 
 % 4) myreverse
+% What source did you use? CHATGPT
+% Why did you use that source? I forgot how to do tail reverse
+% Did using that source benefit you? Yes, I relearned how to do tail reverse
+% Did using that source interfere with your understanding in any way? No
 myreverse(List) -> myreverse(List, []).
-
 myreverse([], Acc) -> Acc;
 myreverse([H|T], Acc) -> myreverse(T, [H|Acc]).
 
 % 5) myreplaceall
+% What source did you use? CHATGPT
+% Why did you use that source? I forgot how to pattern match the old element in the list
+% Did using that source benefit you? Yes, I learned how to pattern match old element in the list
+% Did using that source interfere with your understanding in any way? No
 myreplaceall(_, _, []) -> [];
 myreplaceall(New, Old, [Old|T]) -> [New | myreplaceall(New, Old, T)];
 myreplaceall(New, Old, [H|T]) -> [H | myreplaceall(New, Old, T)].
